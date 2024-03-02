8c19d9b147a75ec9d52499ead9d1c102fec9862b
SOMOGYI Arnold <arnold.somogyi@ext.ec.europa.eu>
2024-03-02 02:12:32 +0000

    working template rest service

---
+++ modules/rest-template/src/main/java/com/remal/gombi/template/rest/Application.java
@@ -0,0 +1,22 @@
+/*
+ *  Copyright (c) 2020-2024 Remal Software and Arnold Somogyi All rights reserved
+ *
+ *  Since: February 2024
+ *  Author: Arnold Somogyi <arnold.somogyi@gmail.com>
+ *
+ *  Description:
+ *     <class-description>
+ */
+
+package com.remal.gombi.template.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ modules/rest-template/src/main/java/com/remal/gombi/template/rest/controller/HelloController.java
@@ -0,0 +1,40 @@
+/*
+ *  Copyright (c) 2020-2024 Remal Software and Arnold Somogyi All rights reserved
+ *
+ *  Since: February 2024
+ *  Author: Arnold Somogyi <arnold.somogyi@gmail.com>
+ *
+ *  Description:
+ *     Spring REST endpoint.
+ */
+
+package com.remal.gombi.template.rest.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/hello")
+public class HelloController {
+
+    @Value("${app.hello.name}")
+    private String hello;
+
+    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyy-MM-dd HH:mm:ss");
+    @GetMapping
+    public String sayHello() {
+        var response = String.format("Hello %s, the time is %s.",
+                hello,
+                LocalDateTime.now().format(DATE_TIME_FORMATTER));
+
+        log.debug(response);
+        return response;
+    }
+}
