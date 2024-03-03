6514f5d6a8ed3e1ec61ea92559b8dd675b6765b0
SOMOGYI Arnold <arnold.somogyi@ext.ec.europa.eu>
2024-03-03 14:10:09 +0000

    update java-runner17: run JARs in a loop and check exit code

--- docker/application/jar-runner-17/container-scripts/startup/40151_run-jars.sh
+++ docker/application/jar-runner-17/container-scripts/startup/40151_run-jars.sh
@@ -15,15 +15,27 @@
 log_start "$0"
 
 number_of_jars=$(find "$JAR_HOME" -maxdepth 1 -type f -name "*.jar" | wc -l)
-printf "JAR files in the %s directory: %s\n" "$JAR_HOME" "$number_of_jars"
+printf "%s | [DEBUG] JAR files in the %s directory: %s\n" "$(date +"%Y-%b-%d %H:%M:%S")" "$JAR_HOME" "$number_of_jars"
 
 if [[ "$number_of_jars" -eq 0 ]]; then
   printf "%s | [INFO]  there is no JAR file to execute\n" "$(date +"%Y-%b-%d %H:%M:%S")"
 else
   cd "$JAR_HOME" || { echo "Error while trying to change directory from $(pwd) to $JAR_HOME"; exit 1; }
+
   for jar_file in "$JAR_HOME"/*.jar; do
-    printf "%s | [INFO]  running %s at the background...\n" "$(date +"%Y-%b-%d %H:%M:%S")" "$jar_file"
+    printf "%s | [INFO]  starting %s at the background...\n" "$(date +"%Y-%b-%d %H:%M:%S")" "$jar_file"
     nohup java -jar "$jar_file" 2>&1 &
+    exit_code=$?
+    pid=$!
+    printf "%s | [DEBUG] PID       of the %s is %s\n" "$(date +"%Y-%b-%d %H:%M:%S")" "$jar_file" "$pid"
+    printf "%s | [DEBUG] Exit code of the %s is %s\n" "$(date +"%Y-%b-%d %H:%M:%S")" "$jar_file" "$exit_code"
+
+    timeout 5 tail --pid=$pid -f /dev/null
+    exit_code=$?
+    printf "%s | [DEBUG] Exit code of the %s is %s\n" "$(date +"%Y-%b-%d %H:%M:%S")" "$jar_file" "$exit_code"
+    if [ "$exit_code" -ne 0 ]; then
+       printf "%s | [ERROR] Java application %s exited with an error: %s" "$(date +"%Y-%b-%d %H:%M:%S")" "$jar_file" "$exit_code"
+    fi
   done
 fi
 
