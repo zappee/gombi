9a722354809ac7e0b1eee0efe317e11ec8bd33fe
SOMOGYI Arnold <arnold.somogyi@ext.ec.europa.eu>
2024-03-05 23:47:42 +0000

    adding consul service discovery, but it does not work yet

--- modules/rest-template/src/main/java/com/remal/gombi/template/rest/Application.java
+++ modules/rest-template/src/main/java/com/remal/gombi/template/rest/Application.java
@@ -10,13 +10,14 @@
 
 package com.remal.gombi.template.rest;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        new SpringApplicationBuilder(Application.class).web(WebApplicationType.SERVLET).run(args);
     }
 }
