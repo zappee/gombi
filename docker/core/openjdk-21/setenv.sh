cf414f180c9aff9b07b4c107e978a22b5c2161b1
SOMOGYI Arnold <arnold.somogyi@ext.ec.europa.eu>
2024-03-22 12:23:04 +0100

    adding openjdk-21 image

--- docker/core/openjdk-21/setenv.sh
+++ docker/core/openjdk-21/setenv.sh
@@ -2,14 +2,14 @@
 # ******************************************************************************
 # Environment file to define variables used during the Docker image build.
 #
-# Since : January 2023
+# Since : March 2024
 # Author: Arnold Somogyi <arnold.somogyi@gmail.com>
 #
-# Copyright (c) 2020-2023 Remal Software and Arnold Somogyi All rights reserved
+# Copyright (c) 2020-2024 Remal Software and Arnold Somogyi All rights reserved
 # ******************************************************************************
-export IMAGE_NAME="remal-openjdk-17"
+export IMAGE_NAME="remal-openjdk-21"
 export IMAGE_TAG="0.0.1"
-export IMAGE_DESCRIPTION="OpenJDK 17 Docker image"
+export IMAGE_DESCRIPTION="OpenJDK 21 Docker image"
 export IMAGE_AUTHOR="Arnold Somogyi <arnold.somogyi@gmail.com>"
 export IMAGE_FROM="remal-base:0.0.1"
 export BUILD_TYPE="${1:-fat}"
