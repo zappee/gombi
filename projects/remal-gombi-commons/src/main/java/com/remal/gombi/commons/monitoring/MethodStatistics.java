3d714699d72290488a291e94bcdace2b7f131ae0
Arnold SOMOGYI <arnold.somogyi@gmail.com>
2025-01-16 17:00:50 +0000

    improve method end log

--- projects/remal-gombi-commons/src/main/java/com/remal/gombi/commons/monitoring/MethodStatistics.java
+++ projects/remal-gombi-commons/src/main/java/com/remal/gombi/commons/monitoring/MethodStatistics.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright (c) 2020-2024 Remal Software and Arnold Somogyi All rights reserved
+ *  Copyright (c) 2020-2025 Remal Software and Arnold Somogyi All rights reserved
  *
  *  Since:  March 2024
  *  Author: Arnold Somogyi <arnold.somogyi@gmail.com>
@@ -7,7 +7,7 @@
  *  Description:
  *     Custom annotation to log the execution time of a method.
  */
-package com.remal.gombi.hello.commons.monitoring;
+package com.remal.gombi.commons.monitoring;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
